8dea0c2e1ac33e35edc731c7f1a5875b8e4ea482
meetmarkli <meetmarkli@gmail.com>
2021-09-29 20:37:49 +1000

    port

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const connectToDB = require('./utils/db');
 
 connectToDB();
 
-app.listen(3000, () => {
-  console.log('server listening on port 3000');
+const {PORT = 3000} = process.env
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
 });
